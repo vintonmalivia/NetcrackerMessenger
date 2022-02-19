c9979f25e2dcb0572e1543dcb4ac691ad2a36838
Anton Shvechikhin <antonkamski@gmail.com>
2022-02-19 15:09:56 +0400

    Added nested classes with constants

--- src/main/java/com/messenger/controllers/ConversationController.java
+++ src/main/java/com/messenger/controllers/ConversationController.java
@@ -4,49 +4,69 @@ import com.messenger.repository.IDatabaseConversationDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import static com.messenger.constants.Endpoints.CONVERSATIONS;
-import static com.messenger.constants.Endpoints.NEW;
+import org.springframework.web.bind.annotation.*;
+import static com.messenger.constants.controllers.Endpoints.CONVERSATIONS;
+import static com.messenger.constants.controllers.Endpoints.NEW;
 
 @Controller
 @RequestMapping(CONVERSATIONS)
 public class ConversationController
 {
-    private static final String CONVERSATIONS_PATH = "conversations/";
-    private static final String ALL_CONVERSATIONS = "allConversations";
-    private static final String ALL_CONVERSATIONS_HTML = "allConversations";
-    private static final String NEW_CONVERSATION = "newConversation";
-    private static final String NEW_CONVERSATION_HTML = "newConversation";
-    private static final String REDIRECT = "redirect:/";
+//    private static final String CONVERSATIONS_PATH = "conversations/";
+//    private static final String ALL_CONVERSATIONS = "allConversations";
+//    private static final String ALL_CONVERSATIONS_HTML = "allConversations";
+//    private static final String NEW_CONVERSATION = "newConversation";
+//    private static final String NEW_CONVERSATION_HTML = "newConversation";
+//    private static final String REDIRECT = "redirect:/";
+
+    // Пример объявления вложенных статических классов констант
+    private static abstract class Views {
+        private static final String CONVERSATIONS_PATH = "conversations/";
+        private static final String ALL_CONVERSATIONS_HTML = "allConversations";
+        private static final String NEW_CONVERSATION_HTML = "newConversation";
+        private static final String REDIRECT = "redirect:/";
+    }
+
+    private static abstract class ModelAttributes {
+        private static final String ALL_CONVERSATIONS = "allConversations";
+        private static final String NEW_CONVERSATION = "newConversation";
+        private static final String CONVERSATION = "com/messenger/exceptions/conversation";
+    }
+
     @Autowired
     private  IDatabaseConversationDAO databaseConversationDAO;
 
     @GetMapping
     public String getConversations(Model model)
     {
-        model.addAttribute(ALL_CONVERSATIONS, /*ConversationManager.getInstance().getConversations()*/
+        model.addAttribute(ModelAttributes.ALL_CONVERSATIONS, /*ConversationManager.getInstance().getConversations()*/
                 databaseConversationDAO.findAll());
-        return CONVERSATIONS_PATH + ALL_CONVERSATIONS_HTML;
+        return Views.CONVERSATIONS_PATH + Views.ALL_CONVERSATIONS_HTML;
     }
 
     @GetMapping(NEW)
-    public String getPageForCreateNewConversation(@ModelAttribute(NEW_CONVERSATION) Conversation conversation)
+    public String getPageForCreateNewConversation(@ModelAttribute(ModelAttributes.NEW_CONVERSATION) Conversation conversation)
     {
-        return CONVERSATIONS_PATH + NEW_CONVERSATION_HTML;
+        return Views.CONVERSATIONS_PATH + Views.NEW_CONVERSATION_HTML;
     }
 
     @PostMapping
-    public String createNewConversation(@ModelAttribute(NEW_CONVERSATION) Conversation conversation)
+    public String createNewConversation(@ModelAttribute(ModelAttributes.NEW_CONVERSATION) Conversation conversation)
     {
 //        List<Conversation> conversations = ConversationManager.getInstance().getConversations();
 //        conversation.setId(UUID.randomUUID());
 //        conversations.add(conversation);
 //        ConversationManager.getInstance().setConversations(conversations);
+//        textMessageDAO.saveNewConversation(conversation);
         databaseConversationDAO.save(conversation);
-        //textMessageDAO.saveNewConversation(conversation);
-        return REDIRECT + CONVERSATIONS_PATH;
+        return Views.REDIRECT + Views.CONVERSATIONS_PATH;
     }
+
+    @DeleteMapping
+    public String deleteConversation(@ModelAttribute(ModelAttributes.CONVERSATION) Conversation conversation)
+    {
+        databaseConversationDAO.delete(conversation);
+        return Views.REDIRECT + Views.CONVERSATIONS_PATH;
+    }
+
 }
--- src/main/java/com/messenger/controllers/MessagesController.java
+++ src/main/java/com/messenger/controllers/MessagesController.java
@@ -1,6 +1,4 @@
 package com.messenger.controllers;
-import com.messenger.models.Conversation;
-import com.messenger.repository.ConversationManager;
 import com.messenger.repository.IDatabaseMessageDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,10 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Collections;
-import java.util.List;
+import javax.sql.DataSource;
 import java.util.UUID;
-import static com.messenger.constants.Endpoints.MESSAGES;
+import static com.messenger.constants.controllers.Endpoints.MESSAGES;
 
 @Controller
 @RequestMapping(MESSAGES)
@@ -24,15 +21,11 @@ public class MessagesController
 //    public MessagesController(TextMessageDAO textMessageDAO){
 //        this.textMessageDAO = textMessageDAO;
 //    }
-    private static final String CONVERSATIONS_PATH = "conversations/"; // TODO: Можно объединять константы для разных целей в отдельные вложенные статические классы (пример сразу после констант)
-    private static final String MESSAGES = "conversationMessages";
-    private static final String MESSAGES_HTML = "conversationMessages";
-    private static final String UUID = "uuid";
 
     // Пример объявления вложенных статических классов констант
-    /*
     private static abstract class Views {
-        public static final String CONVERSATION_MESSAGES = "conversations/conversationMessages";
+        private static final String CONVERSATIONS_PATH = "conversations/";
+        private static final String MESSAGES_HTML = "conversationMessages";
     }
 
     private static abstract class PathVariables {
@@ -40,20 +33,19 @@ public class MessagesController
     }
 
     private static abstract class ModelAttributes {
-        public static final String SOME_MODEL_ATTRIBUTE = "some_attribute";
+        private static final String MESSAGES = "conversationMessages";
     }
-    */
 
     @Autowired
     private IDatabaseMessageDAO databaseMessageDAO;
 
     @GetMapping
-    public String getMessagesFromConversationByID(@PathVariable(UUID) UUID uuid, Model model)
+    public String getMessagesFromConversationByID(@PathVariable(PathVariables.UUID) UUID uuid, Model model)
     {
 //        List<Conversation> conversations = ConversationManager.getInstance().getConversations();
 //        Conversation resultConversation = conversations.stream().filter(conversation -> conversation.getId().equals(uuid)).findAny().orElse(null);
 //        model.addAttribute(MESSAGES, resultConversation);
-//        model.addAttribute(MESSAGES, databaseMessageDAO.findAllById());
-        return CONVERSATIONS_PATH + MESSAGES_HTML;
+        model.addAttribute(ModelAttributes.MESSAGES, databaseMessageDAO.getMessages(uuid));
+        return Views.CONVERSATIONS_PATH + Views.MESSAGES_HTML;
     }
 }
