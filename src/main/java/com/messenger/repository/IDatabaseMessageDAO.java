065d583fcdb19d453fbca184928682233ac92c67
Anton Shvechikhin <antonkamski@gmail.com>
2022-02-19 15:10:53 +0400

    Added @Query

--- src/main/java/com/messenger/repository/IDatabaseMessageDAO.java
+++ src/main/java/com/messenger/repository/IDatabaseMessageDAO.java
@@ -1,12 +1,16 @@
 package com.messenger.repository;
-
 import com.messenger.models.AbstractMessage;
+import com.messenger.models.impl.TextMessage;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 import java.util.UUID;
 
 /* Todo: (в дискорде про это уже сказано) Научиться делать методы, которые не входят в обычный CrudRepository */
 /*  Например: поиск всех сообщений, принадлежащих одному чату (по ID чата). Подсказка: Посмотри про аннотации @Query */
-public interface IDatabaseMessageDAO extends CrudRepository<AbstractMessage, UUID> {
+public interface IDatabaseMessageDAO extends CrudRepository<TextMessage, UUID> {
 
+    @Query(value = "SELECT * FROM text_messages WHERE id_conversation = CAST (:uuid AS varchar)", nativeQuery = true)
+    List<TextMessage> getMessages(@Param("uuid") UUID uuid);
 }
