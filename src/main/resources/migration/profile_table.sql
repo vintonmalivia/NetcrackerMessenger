326d19a075ffa52cec5b4529ec132dc236d91cb5
Anton Shvechikhin <antonkamski@gmail.com>
2022-04-13 19:37:26 +0400

    Added profile_id column

--- src/main/resources/migration/profile_table.sql
+++ src/main/resources/migration/profile_table.sql
@@ -2,7 +2,5 @@ create table if not exists profile
 (
     id      varchar PRIMARY KEY,
     name    varchar NOT NULL,
-    surname varchar NOT NULL,
-    user_id varchar NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES table_user (id)
+    surname varchar NOT NULL
 );
--- src/main/resources/migration/user_table.sql
+++ src/main/resources/migration/user_table.sql
@@ -2,5 +2,6 @@ create table if not exists table_user
 (
     id       varchar      not null primary key,
     password varchar(255) not null,
-    username varchar(255) not null
+    username varchar(255) not null,
+    profile_id varchar(255) not null
 );
