bb1177b88804940b163db7cfa87f21e8b8f2d91f
Anton Shvechikhin <antonkamski@gmail.com>
2022-04-16 11:51:57 +0400

    Added role initialization

--- src/main/resources/migration/role_table.sql
+++ src/main/resources/migration/role_table.sql
@@ -3,3 +3,9 @@ create table if not exists table_role
     id   varchar not null primary key,
     name varchar(255)
 );
+
+INSERT INTO table_role VALUES ('0ada0e40-c34b-48ce-9ddc-be67eec99eed', 'ROLE_USER')
+ON CONFLICT DO NOTHING;
+
+INSERT INTO table_role VALUES ('ca7c0960-9cbf-11ec-b909-0242ac120002', 'ROLE_ADMIN')
+ON CONFLICT DO NOTHING;
