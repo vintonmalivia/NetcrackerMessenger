73000ac77f08a2977b03d4b765e14e0f4de497b6
Anton Shvechikhin <antonkamski@gmail.com>
2022-04-19 17:42:36 +0400

    Added adding users to the conversation

--- src/main/java/com/messenger/controllers/MessagesController.java
+++ src/main/java/com/messenger/controllers/MessagesController.java
@@ -1,7 +1,11 @@
 package com.messenger.controllers;
 
+import com.messenger.models.User;
 import com.messenger.models.impl.TextMessage;
+import com.messenger.repository.UserRepository;
+import com.messenger.service.ConversationService;
 import com.messenger.service.MessageService;
+import com.messenger.service.ProfileService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+import static com.messenger.constants.controllers.Endpoints.ADD_NEW_MEMBER;
 import static com.messenger.constants.controllers.Endpoints.MESSAGES;
 
 @Controller
@@ -20,6 +25,7 @@ public class MessagesController
     private static abstract class Views {
         private static final String CONVERSATIONS_PATH = "conversations/";
         private static final String MESSAGES_HTML = "conversationMessages";
+        private static final String ADD_NEW_MEMBER_HTML = "addNewMember";
     }
 
     private static abstract class PathVariables {
@@ -29,6 +35,14 @@ public class MessagesController
     private static abstract class ModelAttributes {
         private static final String MESSAGES = "conversationMessages";
         private static final String NEW_MESSAGE = "newMessage";
+        private static final String NEW_MEMBER = "newMember";
+
+        private static final String USER_NOT_EXIST = "userNotExist";
+        private static final String USER_NOT_EXIST_VALUE = "User not exist.";
+        private static final String USER_ALREADY_IN_CONVERSATION = "userAlreadyInConversation";
+        private static final String USER_ALREADY_IN_CONVERSATION_VALUE = "User already in this conversation.";
+        private static final String USER_ADDED = "userAdded";
+        private static final String USER_ADDED_VALUE = "User successfully added to this conversation.";
     }
 
     private static abstract class Redirects {
@@ -38,11 +52,18 @@ public class MessagesController
     private static final Logger logger = LoggerFactory.getLogger(MessagesController.class);
 
     private final MessageService messageService;
+    private final UserRepository userRepository;
+    private final ConversationService conversationService;
+
 
     @Autowired
-    public MessagesController(MessageService messageService)
+    public MessagesController(MessageService messageService,
+                              UserRepository userRepository,
+                              ConversationService conversationService, ProfileService profileService)
     {
-        this.messageService=messageService;
+        this.messageService = messageService;
+        this.userRepository = userRepository;
+        this.conversationService = conversationService;
     }
 
     @GetMapping
@@ -50,7 +71,7 @@ public class MessagesController
                                                   @ModelAttribute(ModelAttributes.NEW_MESSAGE) TextMessage textMessage,
                                                   Model model)
     {
-        model.addAttribute(ModelAttributes.MESSAGES, messageService.getTextMessages(uuid));
+        model.addAttribute(ModelAttributes.MESSAGES, messageService.getMessages(uuid));
         return Views.CONVERSATIONS_PATH + Views.MESSAGES_HTML;
     }
 
@@ -62,7 +83,37 @@ public class MessagesController
         messageService.create(textMessage, uuid);
         logger.info("Message with ID = {} by user with profile ID = {} has been sent.",
                 textMessage.getId(), textMessage.getSender().getUserID());
-        model.addAttribute(ModelAttributes.MESSAGES, messageService.getTextMessages(uuid));
+        model.addAttribute(ModelAttributes.MESSAGES, messageService.getMessages(uuid));
         return Redirects.REDIRECT_TO_CONVERSATION_MESSAGES;
+    }
+
+    @GetMapping(ADD_NEW_MEMBER)
+    public String getPageToAddMember(@ModelAttribute(ModelAttributes.NEW_MEMBER) User user,
+                                     @PathVariable UUID uuid)
+    {
+        return Views.CONVERSATIONS_PATH + Views.ADD_NEW_MEMBER_HTML;
+    }
+
+    @PostMapping(ADD_NEW_MEMBER)
+    public String addMemberToConversation(@ModelAttribute(ModelAttributes.NEW_MEMBER) User user,
+                                          @PathVariable UUID uuid,
+                                          Model model)
+    {
+        if (!userRepository.existsByUsername(user.getUsername()))
+        {
+            model.addAttribute(ModelAttributes.USER_NOT_EXIST, ModelAttributes.USER_NOT_EXIST_VALUE);
+            return Views.CONVERSATIONS_PATH + Views.ADD_NEW_MEMBER_HTML;
+        }
+
+        if (conversationService.existenceOfUserInConversation(uuid, user))
+        {
+            model.addAttribute(ModelAttributes.USER_ALREADY_IN_CONVERSATION,
+                    ModelAttributes.USER_ALREADY_IN_CONVERSATION_VALUE);
+            return Views.CONVERSATIONS_PATH + Views.ADD_NEW_MEMBER_HTML;
+        }
+
+        conversationService.addMemberToConversation(uuid, user);
+        model.addAttribute(ModelAttributes.USER_ADDED, ModelAttributes.USER_ADDED_VALUE);
+        return Views.CONVERSATIONS_PATH + Views.ADD_NEW_MEMBER_HTML;
     }
 }
