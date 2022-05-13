70e502fa9d9117ae19087cc043dae35649c6a232
Anton Shvechikhin <antonkamski@gmail.com>
2022-05-13 14:04:30 +0400

    Added method for getting number of messages in last minute by user

--- src/main/java/com/messenger/repository/MessageRepository.java
+++ src/main/java/com/messenger/repository/MessageRepository.java
@@ -15,4 +15,9 @@ public interface MessageRepository extends CrudRepository<TextMessage, UUID> {
     @Query(value = "SELECT * FROM text_messages WHERE id_conversation = CAST (:convID AS varchar) ORDER BY date",
             nativeQuery = true)
     List<TextMessage> getMessages(@Param("convID") UUID convID);
+
+    @Query(value = "SELECT count(*) FROM abstract_messages WHERE sender_id = CAST (:senderID AS varchar) " +
+            "AND CAST(date AS timestamp) BETWEEN (now() - INTERVAL '1 minute') and now()",
+            nativeQuery = true)
+    int getNumberOfMessagesInLastMinute(@Param("senderID") UUID senderID);
 }
